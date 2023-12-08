5f4c6dcd0d6ba127e90c799a1dbcf1f9198a0f65
leocabs29 <152485105+leocabs29@users.noreply.github.com>
2023-12-09 01:21:26 +0800

    update

--- script.js
+++ script.js
@@ -6,3 +6,49 @@ function drop() {
         drop.style.display = "none";
     }
 }
+import express from 'express';
+import { json } from 'body-parser';
+import { createPool } from 'mysql';
+
+const app = express();
+const port = 3000;
+
+const pool = createPool({
+  host: 'Information Web',
+  user: 'root',
+  password: '1234',
+  database: 'infoweb',
+});
+
+app.use(json());
+
+app.post('/signup', (req, res) => {
+  const { name, program, campus, email } = req.body;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting MySQL connection: ', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const sql = 'INSERT INTO users (name, program, campus, email) VALUES (?, ?, ?, ?)';
+    const values = [name, program, campus, email];
+
+    connection.query(sql, values, (queryError, results) => {
+      connection.release();
+
+      if (queryError) {
+        console.error('Error executing MySQL query: ', queryError);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      res.status(200).send('Signup successful');
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
