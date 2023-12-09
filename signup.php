697f9c92bc2628e151c3a872ea048b135ca576a5
ItsMeKayden <152764702+ItsMeKayden@users.noreply.github.com>
2023-12-09 17:08:43 +0800

    stfu

---
+++ signup.php
@@ -0,0 +1,27 @@
+<?php
+
+    $name = $_POST['name'];
+    $program = $_POST['program'];
+    $campus = $_POST['campus'];
+    $email = $_POST['email'];
+
+    //database connection
+    $conn = new mysqli('localhost', 'root', 'Hanaban28', 'signin');
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        $wkwk = $conn->prepare("INSERT INTO signup (Name, Program, Campus, Email) VALUES (?, ?, ?, ?)");
+        $wkwk->bind_param("ssss", $name, $program, $campus, $email);
+
+        // execute the query
+        if ($wkwk->execute()) {
+            echo "<script>alert('Sign up Success');</script>";
+        } else {
+            echo "Error: " . $wkwk->error;
+        }
+
+        $wkwk->close();
+        $conn->close();
+    }
+?>
